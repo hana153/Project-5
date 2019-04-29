05ea2bc4d00720f3e7e77cad79be024080fb68f6
Hana <hanabananak@gmail.com>
2019-04-29 15:05:38 -0500

    added the JFrame basics to the MesonetFrame class

--- src/MesonetFrame.java
+++ src/MesonetFrame.java
@@ -1,3 +1,5 @@
+import javax.swing.JFrame;
+
 /**
  * Class to create a GUI to accept the given input and relay the calculated information from that. This uses a 
  * Mesonet.txt file to find the given stations with the parameters inputed by the user. The user can also add
@@ -7,7 +9,23 @@
  * @version 2019-04-29
  *
  */
-public class MesonetFrame
+public class MesonetFrame extends JFrame
 {
 
+    /**
+     * Constructor for the MesonetFrame class. 
+     */
+    public MesonetFrame()
+    {
+        // basics for the initial JFrame
+        this.setTitle("Hamming Distance");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(600, 800);
+        this.setVisible(true);
+    }
+    
+    public static void main(String[]args)
+    {
+        MesonetFrame test = new MesonetFrame();
+    }
 }
