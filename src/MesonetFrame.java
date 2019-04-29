995959704f3ecdb00d7318834c9d3ac23711603f
Hana <hanabananak@gmail.com>
2019-04-29 13:23:26 -0500

    initialized project and created class

---
+++ src/MesonetFrame.java
@@ -0,0 +1,13 @@
+/**
+ * Class to create a GUI to accept the given input and relay the calculated information from that. This uses a 
+ * Mesonet.txt file to find the given stations with the parameters inputed by the user. The user can also add
+ * a station to the given list and there is a section where _____ can occur.
+ * 
+ * @author Hana Stevenson
+ * @version 2019-04-29
+ *
+ */
+public class MesonetFrame
+{
+
+}
